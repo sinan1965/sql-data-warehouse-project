6e42cee9ed59cf05dcd1565219adff8dda20e7a9
sinan1965 <105199090+sinan1965@users.noreply.github.com>
2025-11-09 12:37:05 +0300

    Update ddl.silver.sql

--- scripts/silver/ddl.silver.sql
+++ scripts/silver/ddl.silver.sql
@@ -1,1 +1,10 @@
-
+/*
+===================================================================================
+DDL Script: Create Silver Layer Tables
+===================================================================================
+Script Purpose:
+    This script creates tables in the Silver schema, dropping existing tables 
+    if they already exist.
+    Run this script to re-define the DDL structure of Silver layer tables
+===================================================================================
+*/

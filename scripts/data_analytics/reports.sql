c2a0ad1b8f324d1e5422f4ec76897fab4f197f26
sinan1965 <105199090+sinan1965@users.noreply.github.com>
2025-11-12 01:08:13 +0300

    Update reports.sql

--- scripts/data_analytics/reports.sql
+++ scripts/data_analytics/reports.sql
@@ -1,1 +1,124 @@
 
+/*
+=================================================
+Customer Report
+=================================================
+Purpose:
+	-This report consolidates key customer metrics and behaviours
+
+Highlights:
+	1. Gather essential fields such as names, ages and transaction details.
+	2. Segments customers into categories (VIP, Regular, New) and age groups.
+	3. Aggregates customer-level metrics:
+		- total orders,
+		- total sales,
+		- total quantity purchased,
+		- total products,
+		- lifespan in months.
+	4. Calculates valuable KPI's:
+		- recency (months since last order)
+		- average order value
+		- average monthly spend
+==========================================================
+*/
+
+create view gold.report_customer as
+--Base query
+with base_query as(
+select 
+f.order_number,
+f.product_key,
+f.order_date,
+f.sales_amount,
+f.quantity,
+c.customer_key,
+c.customer_number,
+concat(c.first_name, ' ', c.last_name) as customer_name,
+datediff(year, c.birth_date, getdate()) as customer_age
+from gold.fact_sales f
+left join gold.dim_customers c
+on f.customer_key = c.customer_key
+where order_date is not null)
+
+,customer_aggregations as(
+--customer_level metrics
+select 
+customer_key,
+customer_number,
+customer_name,
+customer_age,
+count(distinct order_number) as total_orders,
+sum(sales_amount) as total_sales,
+sum(quantity) as total_quantity,
+count(distinct product_key) as total_product,
+max(order_date) as last_order_date,
+datediff(month, min(order_date), max(order_date)) as lifespan
+from base_query
+group by 
+customer_key,
+customer_number,
+customer_name,
+customer_age
+)
+
+select
+customer_key,
+customer_number,
+customer_name,
+customer_age,
+case when customer_age < 20 then 'Under 20'
+	 when customer_age between 20 and 20 then '20-29'
+	 when customer_age between 30 and 39 then '30-39'
+	 when customer_age between 40 and 49 then '40-49'
+	 else '50 and above'
+end as age_group, 
+case when lifespan >= 12 and total_sales > 5000 then 'VIP'
+	 when lifespan >= 12 and total_sales <= 5000 then 'Regular'
+	 else 'New'
+end customer_segments,
+last_order_date,
+datediff(month, last_order_date, getdate()) as recency,
+total_orders,
+total_sales,
+total_quantity,
+total_product,
+lifespan,
+--compute average order value(AVO)
+case when total_orders = 0 then 0
+	 else total_sales / total_orders 
+end as avg_order_value, 
+--compute average monthly spend
+case when lifespan = 0 then total_sales
+	 else total_sales / lifespan
+end as avg_monthly_spend
+from customer_aggregations
+
+select
+* 
+from gold.report_customer;
+
+
+/*
+=================================================
+Product Report
+=================================================
+Purpose:
+	-This report consolidates key product metrics and behaviours
+
+Highlights:
+	1. Gathers essential fields such as product name, category, subcategory and cost.
+	2. Segments products by revenue to identify hihg perfromers. 
+	3. Aggregates product-level metrics:
+		- total orders,
+		- total sales,
+		- total quantity sold,
+		- total customers,
+		- lifespan in months.
+	4. Calculates valuable KPI's:
+		- recency (months since last sale)
+		- average order revenue
+		- average monthly revenue
+==========================================================
+*/
+
+
